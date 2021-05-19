2d09534a5230f2576f16ae935b676cb68db96bfa
BrianJSo <brian_jezreel_so@dlsu.edu.ph>
2021-05-19 22:48:17 +0800

    brian commit

--- app/src/main/java/com/mobdeve/caim_sob/questlogbook/MainActivity.java
+++ app/src/main/java/com/mobdeve/caim_sob/questlogbook/MainActivity.java
@@ -1,17 +1,26 @@
 package com.mobdeve.caim_sob.questlogbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button questMakerBtn;
+    private Button toQuestMakerBtn;
+    private FloatingActionButton toQuickCreateBtn;
+    private RecyclerView activeQuestsRv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.toQuestMakerBtn = findViewById(R.id.toQuestMakerBtn);
+        this.toQuickCreateBtn = findViewById(R.id.toQuickCreateBtn);
+        this.activeQuestsRv = findViewById(R.id.activeQuestsRv);
     }
 }
