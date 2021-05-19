0f1a8fce3322d4b399a6eaacd3f43a62205473c0
BrianJSo <brian_jezreel_so@dlsu.edu.ph>
2021-05-20 00:17:03 +0800

    brian commit

--- app/src/main/java/com/mobdeve/caim_sob/questlogbook/MainActivity.java
+++ app/src/main/java/com/mobdeve/caim_sob/questlogbook/MainActivity.java
@@ -3,7 +3,9 @@ package com.mobdeve.caim_sob.questlogbook;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -22,5 +24,13 @@ public class MainActivity extends AppCompatActivity {
         this.toQuestMakerBtn = findViewById(R.id.toQuestMakerBtn);
         this.toQuickCreateBtn = findViewById(R.id.toQuickCreateBtn);
         this.activeQuestsRv = findViewById(R.id.activeQuestsRv);
+
+        this.toQuestMakerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, QuestMakerForm.class);
+                startActivity(i);
+            }
+        });
     }
 }
