0f5112c54a2531384fa472f3da5d3122b4c6ae2a
CaiMarkJayson <mark_cai@dlsu.edu.ph>
2021-05-21 00:52:07 +0800

    quest maker form class: quick create dialog

--- app/src/main/java/com/mobdeve/caim_sob/questlogbook/QuestViewHolder.java
+++ app/src/main/java/com/mobdeve/caim_sob/questlogbook/QuestViewHolder.java
@@ -1,10 +1,14 @@
 package com.mobdeve.caim_sob.questlogbook;
 
 import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class QuestViewHolder extends RecyclerView.ViewHolder {
@@ -23,8 +27,19 @@ public class QuestViewHolder extends RecyclerView.ViewHolder {
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(itemView.getContext(), QuestActivity.class);
-                itemView.getContext().startActivity(i);
+//                Intent i = new Intent(itemView.getContext(), QuestActivity.class);
+//                itemView.getContext().startActivity(i);
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(itemView.getContext());
+// ...Irrelevant code for customizing the buttons and title
+//                LayoutInflater inflater = this.getLayoutInflater();
+                LayoutInflater inflater = LayoutInflater.from(itemView.getContext());
+                View dialogView = inflater.inflate(R.layout.activity_quest, (ViewGroup) itemView, false);
+                dialogBuilder.setView(dialogView);
+
+                EditText editText = (EditText) dialogView.findViewById(R.id.viewQuestNotesTv);
+                editText.setText("test label");
+                AlertDialog alertDialog = dialogBuilder.create();
+                alertDialog.show();
             }
         });
     }
